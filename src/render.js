37920d604c1593116b679d483bc11242d133514a
Leandro <leandrocitygear@icloud.com>
2024-04-27 22:46:32 -0400

    navigation toggle added

--- src/render.js
+++ src/render.js
@@ -1,11 +1,16 @@
 const { ipcRenderer } = require('electron')
 
-// Event listener to open settings window when 'settings' elemnt is clicef
+// Event listener to open setings window when 'settings' elemnt is cl
 document.getElementById('settings').addEventListener('click', () => {
     console.log('Settings clicked');
   ipcRenderer.send('settings');
 });
 
 document.getElementById('menu').addEventListener('click', () => {
-
+const nav = document.getElementById('navigation');
+if (nav.style.display === "block") {
+  nav.style.display = 'none';
+} else {
+  nav.style.display = 'block';
+}
 });
